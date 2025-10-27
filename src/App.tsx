6c6429fe11a952a2d9bca67a8ec9e30643243795
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-28 04:49:25 +0530

    created skeletons for projects

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/Navbar'
+import './index.css'
+
+function App() {
+
+  return (
+   <main>
+    <Navbar/>
+    <Hero/>
+    <Highlights/>
+   </main>
+  )
+}
+
+export default App
