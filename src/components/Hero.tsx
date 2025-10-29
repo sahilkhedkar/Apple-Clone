8f21ccb1d0ca09f563d21505c020636c47502bff
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-10-29 17:10:48 +0530

    created the highlight component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,67 +1,71 @@
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
-import { useEffect, useState } from "react"
-import { heroVideo, smallHeroVideo } from "../utils"
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useState } from "react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-
-  const [videoSrc , setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
 
   const handleVideoSrcSet = () => {
-    if(window.innerWidth < 760) {
-      setVideoSrc(smallHeroVideo)
-    } else{
-      setVideoSrc(heroVideo)
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("resize" , handleVideoSrcSet)
+    window.addEventListener("resize", handleVideoSrcSet);
 
     return () => {
-      window.removeEventListener("resize" , handleVideoSrcSet)
-    }
-  })
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  });
 
   useGSAP(() => {
-    gsap.to("#hero" , {
+    gsap.to("#hero", {
       opacity: 1,
-      delay: 2
-    })
-    gsap.to("#cta" , {
+      delay: 2,
+    });
+    gsap.to("#cta", {
       opacity: 1,
       y: -50,
-      delay: 2
-    })
-  },[])
+      delay: 2,
+    });
+  }, []);
   return (
-    
-      <section className="w-full nav-height bg-black">
-        <div className="h-5/6 w-full flex-center flex-col">
-          <p id="hero" className="hero-title">
-            iPhone 15 Pro
-          </p>
-          <div className="md:w-10/12 w-9/12">
-            <video className="pointer-events-none" id={videoSrc} muted autoPlay playsInline={true}>
-              <source src={videoSrc} type="video/mp4" />
-            </video>
-          </div>
+    <section className="w-full nav-height bg-black">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            id={videoSrc}
+            muted
+            autoPlay
+            playsInline={true}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
         </div>
+      </div>
 
-        <div
+      <div
         id="cta"
         className="flex flex-col items-center opacity-0 translate-y-20"
-        >
-          <a href="#highlights" className="btn"> Buy </a>
-          <p className="font-normal text-xl">
-            From $199/month or $999
-          </p>
+      >
+        <a href="#highlights" className="btn">
+          {" "}
+          Buy{" "}
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
+      </div>
+    </section>
+  );
+};
 
-        </div>
-      </section>
-    
-  )
-}
-
-export default Hero
+export default Hero;
