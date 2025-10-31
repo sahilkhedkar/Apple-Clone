c88645b70939224deebda72ffa56d796ac64b683
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-01 03:06:33 +0530

    updated App.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
-import Hero from './components/Hero'
-import Highlights from './components/Highlights'
-import Model from './components/Model'
-import Navbar from './components/Navbar'
-import './index.css'
+import * as Sentry from '@sentry/react';
 
-function App() {
+const App = () => {
 
   return (
-   <main>
-    <Navbar/>
-    <Hero/>
-    <Highlights/>
-    <Model/>
-   </main>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
