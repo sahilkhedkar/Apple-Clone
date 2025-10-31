7e461b9c76d3d449e32778d06829fc42c4285b53
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-11-01 03:06:15 +0530

    updated main.jsx by installing sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes
+} from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://f6fc454877548eb4c357be1f222a7bad@o4510286076706816.ingest.us.sentry.io/4510286080704512",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>)
